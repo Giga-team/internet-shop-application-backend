9ac18b93033e441a689fba103ba67cfb7c6714a3
Roman Prokopchuk <ghost62727@gmail.com>
2023-05-26 19:47:40 +0300

    Create project

---
+++ internet-shop-api/src/main/java/com/gigateam/internetshopapi/InternetShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.gigateam.internetshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetShopApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InternetShopApiApplication.class, args);
+  }
+
+}
