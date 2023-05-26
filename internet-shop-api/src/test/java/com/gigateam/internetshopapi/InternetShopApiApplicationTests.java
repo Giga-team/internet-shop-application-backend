2b35b688a9b4847617a7d3b0e48bf7f358324064
Roman Prokopchuk <ghost62727@gmail.com>
2023-05-26 20:02:06 +0300

    🚀 Initialize project, add Docker compose for database (#3)
    * Create project
    
    * Configure database for project
    
    * Add PostgreSQL driver

---
+++ internet-shop-api/src/test/java/com/gigateam/internetshopapi/InternetShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigateam.internetshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetShopApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
